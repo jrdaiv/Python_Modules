468234782b2aed6dbfcecacd60afa92d53e4d2aa
jrdaiv <jrdaiv@gmail.com>
2024-06-24 15:04:08 -0700

    Modules Assignment

---
+++ mood_responses.py
@@ -0,0 +1,18 @@
+# Task 1
+
+def mood_response(mood):
+    mood = mood
+    if mood == "Happy":
+        return "\nThats Great To Hear, My Day Inside inside here Is great too! "
+    elif mood == "Sad":
+        return "\nOh No, Chear Up My Day In Here Is Horrible As Well. Im Working Over-Time! "
+    elif mood == "grumpy":
+        return "\nOoo I See Someone Is Grumpy, Lets Cheer You Up! "
+    elif mood == "scared":
+        return "\nOh No Lets Hope It's Not Texas Chainsaw Scary Or I Took A Bite Out Of The Birthday Cake And Hope They Dont Find Me! "
+    elif mood == "Annoyed":
+        return "\nLets Try To Figure Something We Can Both Not Be Annoyed About! "
+    else:
+        return "\nSorry Please Type In Valid Response! "
+
+
